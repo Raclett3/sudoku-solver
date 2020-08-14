bf7027bbdc88a91c54c136c76e21f4f2d095ad38
Raclett3 <im@s283.net>
2020-08-14 11:56:14 +0900

    Add board struct

---
+++ src/board.rs
@@ -0,0 +1,41 @@
+#[derive(std::fmt::Debug)]
+pub struct Board {
+    size: usize,
+    side_length: usize,
+    board: Vec<Vec<Option<usize>>>,
+}
+
+impl Board {
+    pub fn new(size: usize) -> Self {
+        let side_length = size * size;
+        Self {
+            size,
+            side_length,
+            board: vec![vec![None; side_length]; side_length],
+        }
+    }
+
+    pub fn get_number(&self, x: usize, y: usize) -> Option<usize> {
+        if x < self.side_length && y < self.side_length {
+            self.board[x][y]
+        } else {
+            None
+        }
+    }
+
+    pub fn set_number(&mut self, x: usize, y: usize, number: usize) {
+        if x < self.side_length && y < self.side_length {
+            if 0 < number && number <= self.side_length {
+                self.board[x][y] = Some(number);
+            } else {
+                self.board[x][y] = None;
+            }
+        }
+    }
+
+    pub fn remove_number(&mut self, x: usize, y: usize) {
+        if x < self.side_length && y < self.side_length {
+            self.board[x][y] = None;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod board;
+
+use board::Board;
 use std::io::stdin;
 
 fn read_line() -> String {
@@ -15,14 +18,16 @@ fn read_numbers() -> Vec<usize> {
         .collect()
 }
 
-fn parse_input_into_vec(side_length: usize) -> Result<Vec<Vec<usize>>, &'static str> {
-    let board: Vec<Vec<_>> = (0..side_length).map(|_| read_numbers()).collect();
+fn parse_input_into_vec(size: usize) -> Result<Board, &'static str> {
+    let side_length = size * size;
+    let mut board = Board::new(size);
+    let parsed: Vec<Vec<_>> = (0..side_length).map(|_| read_numbers()).collect();
 
-    if board.len() < side_length {
+    if parsed.len() < side_length {
         return Err("Invalid input format: Not enough rows");
     }
 
-    for row in board.iter() {
+    for (x, row) in parsed.iter().enumerate() {
         if row.len() < side_length {
             return Err("Invalid input format: Not enough columns");
         }
@@ -34,6 +39,10 @@ fn parse_input_into_vec(side_length: usize) -> Result<Vec<Vec<usize>>, &'static 
         if row.iter().any(|&x| x > side_length) {
             return Err("Invalid input format: Number out of range");
         }
+
+        for (y, &number) in row.iter().enumerate() {
+            board.set_number(x, y, number);
+        }
     }
 
     Ok(board)
@@ -43,8 +52,7 @@ fn main() {
     println!("Sudoku Solver");
 
     let size = 3usize;
-    let side_length = size * size;
-    let board = parse_input_into_vec(side_length);
+    let board = parse_input_into_vec(size);
 
     match &board {
         Ok(parsed) => println!("{:?}", parsed),
